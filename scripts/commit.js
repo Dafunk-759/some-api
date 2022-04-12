c95b9b33ad780fdd14f8095c97aba68753ad92b6
dafunk <759974271@qq.com>
2022-04-13 03:13:29 +0800

    commit at: 2022/4/13 03:13:25

--- scripts/commit.js
+++ scripts/commit.js
@@ -1,29 +1,22 @@
 import shell from "shelljs"
 
-const pipe = (a, ...funs) =>
-  funs.reduce((val, fn) => fn(val), a)
+const commitMessage = (() => {
+  let s =
+    process.argv[2] ??
+    `commit at: ${new Date().toLocaleString()}`
 
-const chain = fn => preret => {
-  switch (preret.code) {
-    case 0:
-      return fn(preret)
-    case 1:
-      return preret
-  }
+  return `"${s}"`
+})()
+
+const { code } = shell.exec(
+  `
+    npm run format;
+    git add .;
+    git commit -m ${commitMessage};
+  `,
+  { shell: "powershell" }
+)
+
+if (code !== 0) {
+  shell.exit(1)
 }
-
-const commitMessage = pipe(
-  process.argv[2] ??
-    `commit at: ${new Date().toLocaleString()}`,
-  s => `"${s}"`
-)
-
-// changes
-
-console.log(commitMessage)
-
-pipe(
-  shell.exec("npm run format"),
-  chain(() => shell.exec("git add .")),
-  chain(() => shell.exec("git commit -m " + commitMessage))
-)
